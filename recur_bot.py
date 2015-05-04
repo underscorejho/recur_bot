22be80a17cade048d3d46021da1e0c84836dede7
Jared Henry Oviatt <jaredhenryoviatt@gmail.com>
2015-05-03 21:55:21 -0700

    better error handling

--- recur_bot.py
+++ recur_bot.py
@@ -23,6 +23,7 @@ def main():
   TEN_MINUTES = 600
   STOP = 'end'
   ENDED = False
+  ERR = False
 
   username = '/u/' + USER
   print 'username is: ' + username
@@ -32,13 +33,18 @@ def main():
       # loop through all comments
       print 'looking...'
       for comment in praw.helpers.comment_stream(r, 'all', limit=None, verbosity=0):
+        if ERR: # retry error comment *
+          comment = COMMENT
+          ERR = False 
+        COMMENT = comment
+
         if username in comment.body and comment.id not in commented: # look for new calls to username
           print 'found something...'
           # wait until allowed to participate again
           print 'resting...'
           time.sleep( TEN_MINUTES )
 
-          if commented:
+          if commented: # check if it's been ended
             for reply in comment.replies:
               if STOP in reply.body:
                 # stop recurring on current thread
@@ -58,6 +64,12 @@ def main():
           print 'looking...'
     except Exception as err:
       print err
+      ERR = True
+      
 
 if __name__ == '__main__':
   main()
+
+# *
+# PRAW automatically retries requests, but comment_stream will have a new comment (i think)
+# this method retries the comment it was on, but will ignore the current comment
